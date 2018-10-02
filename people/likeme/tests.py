2385cbeb54a5175e91963a81ec116e3f2f670637
new user <aljones15@gmail.com>
2018-10-02 08:44:36 +0000

    basic test cases

--- people/likeme/tests.py
+++ people/likeme/tests.py
@@ -19,3 +19,39 @@ class LikeMeIndexTests(TestCase):
         self.assertIs(type(response), JsonResponse)
         self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
 
+    def test_should_query_by_age(self):
+        response = self.client.get(reverse('likeme:index'), {"age": 30})
+        self.assertIs(response.status_code, 200)
+        self.assertIs(type(response), JsonResponse)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
+
+    def test_should_query_by_latitude(self):
+        response = self.client.get(reverse('likeme:index'), {"latitude": 40.71667})
+        self.assertIs(response.status_code, 200)
+        self.assertIs(type(response), JsonResponse)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
+
+    def test_should_query_by_longitude(self):
+        response = self.client.get(reverse('likeme:index'), {"longitude": 40.71667})
+        self.assertIs(response.status_code, 200)
+        self.assertIs(type(response), JsonResponse)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
+
+    def test_should_query_by_location(self):
+        response = self.client.get(reverse('likeme:index'), {"longitude": 40.71667, "latitude": 40.71667})
+        self.assertIs(response.status_code, 200)
+        self.assertIs(type(response), JsonResponse)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
+
+    def test_should_query_by_monthly_income(self):
+        response = self.client.get(reverse('likeme:index'), {"monthly income": 5132})
+        self.assertIs(response.status_code, 200)
+        self.assertIs(type(response), JsonResponse)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
+
+    def test_should_query_by_experienced(self):
+        response = self.client.get(reverse('likeme:index'), {"experienced": True})
+        self.assertIs(response.status_code, 200)
+        self.assertIs(type(response), JsonResponse)
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
+
