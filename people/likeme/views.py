039c3673c57bf8c70f34bcc7e9bdcecdbe35414d
new user <aljones15@gmail.com>
2018-10-02 08:56:00 +0000

    created data frame from csv

--- people/likeme/views.py
+++ people/likeme/views.py
@@ -1,4 +1,10 @@
 from django.http import JsonResponse
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+csvPath = Path('./data.csv')
+data = pd.read_csv(csvPath)
 
 # Create your views here.
 
