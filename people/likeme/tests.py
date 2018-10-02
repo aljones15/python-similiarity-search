d657c28057cbc84067b2ef547921928f5981f3b1
new user <aljones15@gmail.com>
2018-10-02 08:31:54 +0000

    asserting json responses

--- people/likeme/tests.py
+++ people/likeme/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.http import JsonResponse
 
 # Create your tests here.
 
@@ -8,4 +9,7 @@ class LikeMeIndexTests(TestCase):
 
     def test_should_return_json(self):
         response = self.client.get(reverse('likeme:index'))
-        self.assertIs(response.status_code, 200);
+        self.assertIs(response.status_code, 200)
+        self.assertIs(type(response), JsonResponse)
+        print(type(response.content))
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {"peopleLikeYou": []})
